2cdc9febac7906f5bcf751f3cd3c5a945b73dcb1
Noah Huppert <Noah-Huppert@Noahs-MBP.home>
2015-12-01 22:59:21 -0500

    -node_modules, made it echo request instead of logging it

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var fs = require("fs");
-var winston = require("winston");
 var app = express();
-
-winston.add(winston.transports.File, { filename: 'Log.log' });
 
 app.use(bodyParser.json());
 
-
-app.post("/", function(req, res){
-  var date = Date.now();
-  var fileName = "POST.json";
-
-  console.log("POST / @" + date);
-
-  fs.writeFile(fileName, JSON.stringify({ "HEADERS": req.headers, "BODY": req.body}, undefined, 2), function fileWriteComplete(err){
-    if(err){
-      console.log("     Error writing to file " + err);
-    } else{
-      console.log("     File saved " + fileName);
-      winston.log("info", req);
-    }
-  });
-
-  res.send("Ok");
+app.all("/", function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    http_method: req.method,
+    headers: req.headers,
+    body: req.body
+  }, null, 4));
 });
 
 
