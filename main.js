14d8ba94a40bae9b86b34021352e7f299fc994d1
Noah Huppert <contact@noahh.io>
2019-08-21 12:43:46 -0400

    made print to console, made accept what route

--- main.js
+++ main.js
@@ -4,17 +4,25 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '10mb'}));
 
-app.all("/", function(req, res){
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    http_method: req.method,
-    headers: req.headers,
-    body: req.body
-  }, null, 4));
-});
+app.all("/", debugEndpoint);
+app.all("/*", debugEndpoint);
 
 app.listen(port, function(){
-  console.log("App listening on port " + port);
+    console.log("App listening on port " + port);
 });
+
+function debugEndpoint(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    
+    var respDat = {
+	   http_method: req.method,
+	   headers: req.headers,
+	   body: req.body
+    };
+    var respStr = JSON.stringify(respDat, null, 4);
+    
+    console.log(respDat);
+    res.send(respStr, null, 4);
+}
